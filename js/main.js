7f51abf434aa0b4c991dbf051fee8c5643f7fc63
Emily Boucher <emi.boucher@gmail.com>
2018-04-07 19:29:41 -0400

    add WIP files to project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+// add in: total Votes, total Voters available
+  var quorum = function(  ) {
+    // pass total percentage of votes cast here
+    votes = 1
+    quorumInput = document.getElementById("quorum").innerHTML = 1 + " TEST %";
+  }
+
+  quorum();
+
+
+
+
+});
