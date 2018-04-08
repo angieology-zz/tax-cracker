808eac7f328c58f3f0a28c039d7a30c67d07f66f
Angela Lee <angelalee.one@gmail.com>
2018-04-08 08:05:52 -0400

    add transfer funds wip

--- TaxCracker.sol
+++ TaxCracker.sol
@@ -3,16 +3,31 @@ pragma solidity ^0.4.17;
 
 contract TaxCracker {
     mapping (bytes32 => uint8) public votesReceived;
+    mapping(bytes32 => address) internal optionsAccounts;
     bytes32[] public optionsList;
+    address funder;
+    uint fund;
+
     address ownerAccount;
-    uint256 tokenOwnerId;
+    uint256 voteTokenOwnerId;
     bytes32 ownerName;
 
-    function TaxCracker(bytes32[] options, uint256 ownerId, bytes32 _ownerName) public {
+    function TaxCracker(bytes32[] options, address[] toAccounts, uint256 ownerId, bytes32 _ownerName) public payable {
         optionsList = options;
-        tokenOwnerId = ownerId;
+        for (uint i = 0; i < optionsList.length; i++) {
+            optionsAccounts[optionsList[i]] = toAccounts[i];
+        }
+        voteTokenOwnerId = ownerId;
         ownerName = _ownerName;
         ownerAccount = msg.sender;
+    }
+
+    function fundContract() public payable {
+        msg.sender.transfer(msg.value);
+    }
+    
+    function getContractBalance() public constant returns (uint){
+        return msg.value;    
     }
 
     function totalVotesFor(bytes32 option) view public returns (uint8) {
@@ -34,17 +49,36 @@ contract TaxCracker {
         return false;
     } 
 
-    function getTokenOwner() view public returns(bytes32 name) {
+    function calculateWinner() view public returns(uint winningOptionIndex) {
+        uint highestVote = 0;
+        uint highestVotedOptionIndex = 0;
+        for (uint i = 0; i < optionsList.length; i++) {
+            if (votesReceived[optionsList[i]] >= highestVote) {
+                highestVotedOptionIndex = i;
+                highestVote = votesReceived[optionsList[i]];
+            }
+        }
+        return highestVotedOptionIndex;
+    }
+
+    function transferFunds(uint winningOptionIndex) public returns(uint) {
+        bytes32 winningOption = optionsList[winningOptionIndex];
+        address winningOptionAccount = optionsAccounts[winningOption];
+        winningOptionAccount.transfer(msg.value);
+        return msg.value;
+    }
+
+    function getVoteTokenOwner() view public returns(bytes32 name) {
         return ownerName;
     }
 
-    function transfer(address _to, uint256 _tokenId, bytes32 _name) public {
+    function transferVoteToken(address _to, uint256 _tokenId, bytes32 _name) public {
         ownerAccount = _to;
-        tokenOwnerId = _tokenId;
+        voteTokenOwnerId = _tokenId;
         ownerName = _name;
     }
 
-    function verifyOwner() view public returns(bool) {
+    function verifyVoteTokenOwner() view public returns(bool) {
         if (ownerAccount == msg.sender) {
             return true;
         } else {
